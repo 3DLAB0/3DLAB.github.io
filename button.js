ae1ecf60133dfaf41f80bd75c5e055b6e76d3288
Taiseibutsu@hotmail.com <94119095+3DLAB0@users.noreply.github.com>
2021-12-17 14:52:24 +0100

    s

---
+++ button.js
@@ -0,0 +1,11 @@
+document.body.innerHTML = "DONE";
+
+
+function show_colido_settings(){
+    var x = document.getElementById("tablecontent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}     
---
+++ js/button.js
@@ -0,0 +1,9 @@
+
+function show_colido_settings(){
+    var x = document.getElementById("tablecontent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}     
