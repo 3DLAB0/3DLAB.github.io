576957e29489f735a36f4b98c68a91d75981834b
Taiseibutsu@hotmail.com <94119095+3DLAB0@users.noreply.github.com>
2021-12-20 16:16:03 +0100

    a

--- js/button.js
+++ js/button.js
@@ -1,9 +1,14 @@
 
-function show_colido_settings(){
-    var x = document.getElementById("tablecontent");
+function toogle_button(id){
+    var x = document.getElementById(id);
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }     
+
+function button_clip_copy(id) {
+    navigator.clipboard.writeText(id);
+    alert("Copied the text: " + id);
+  }
