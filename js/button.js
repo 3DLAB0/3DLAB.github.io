76b48456920deb0f17823824828e614b06194042
Taiseibutsu@hotmail.com <94119095+3DLAB0@users.noreply.github.com>
2021-12-21 13:39:19 +0100

    s

--- js/button.js
+++ js/button.js
@@ -13,16 +13,3 @@ function button_clip_copy(id) {
     alert("Copied the text: " + id);
   }
 
-
-
-function set_event() {
-    var active_date = $('#calendar').evoCalendar('getActiveDate');
-    $('#calendar').evoCalendar('addCalendarEvent', {
-        id: '3D Print',
-        name: 'Mom\'s Birthday',
-        description: 'Lorem ipsum dolor sit..',
-        date: 'May 27, 2020',
-        type: 'birthday'
-        });
-    alert("Event on " + active_date);
-  }
---
+++ js/sidebar.js
@@ -0,0 +1,21 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <link rel="stylesheet" href="../css/sidebar.css"type="text/css" >
+    <div class="icon-bar">
+        <a href="../index.html">
+            <img src="../3DLAB.png" alt="Go back to 3DLAB" width="50" height="50">
+        </a>
+        <a href="../html/calendar.html">
+            <img src="../img/log/calendar_w.png" alt="Go to resources" width="50" height="50">
+        </a>
+        <a href="../html/printing.html">
+            <img src="../img/log/3dprint_w.png" alt="Go to resources" width="50" height="50">
+        </a>
+        <a href="../index.html">
+            <img src="../img/log/resources_w.png" alt="Go to resources" width="50" height="50">
+        </a>    
+    </div>
+`;
+
+document.body.appendChild(template.content);
