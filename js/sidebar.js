1071d6c896c32ad58af0ad7a8a0468112a8ae0dd
3DLAB0 <94119095+3DLAB0@users.noreply.github.com>
2022-06-24 13:42:07 +0200

    asd

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,19 +1,19 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
-    <link rel="stylesheet" href="/css/sidebar.css"type="text/css" >
+    <link rel="stylesheet" href="../css/sidebar.css"type="text/css" >
     <div class="icon-bar">
-        <a href="/index.html">
-            <img src="/3DLAB.png" alt="Go back to 3DLAB" width="50" height="50">
+        <a href="../index.html">
+            <img src="../3DLAB.png" alt="Go back to 3DLAB" width="50" height="50">
         </a>
         <a href="/html/calendar.html">
-            <img src="/img/log/calendar_w.png" alt="Go to resources" width="50" height="50">
+            <img src="../img/log/calendar_w.png" alt="Go to resources" width="50" height="50">
         </a>
         <a href="/html/printing.html">
             <img src="../img/log/3dprint_w.png" alt="Go to resources" width="50" height="50">
         </a>
         <a href="/index.html">
-            <img src="/img/log/resources_w.png" alt="Go to resources" width="50" height="50">
+            <img src="../img/log/resources_w.png" alt="Go to resources" width="50" height="50">
         </a>    
     </div>
 `;
